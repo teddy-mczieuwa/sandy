6fc9858163049a8713c80b9ded236c36c8d232b0
teddy <teddymczieuwa@gmail.com>
2018-12-31 22:01:31 +0100

    Initial setup complete

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{msg}}</h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            msg : 'Hello world'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    @import './assets/sass/main.scss';
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+module.exports = {
+    module:{
+        rules: [
+            {test:/\.vue$/, use:'vue-loader'},
+            {test:/\.js$/, exclude:/node_modules/, use:'babel-loader'},
+            {test:/\.scss$/, 
+                use: [
+                    'style-loader',
+                   // MiniCSSExtractPlugin,
+                    'css-loader',
+                    'sass-loader'
+                ]},
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                {
+                    loader: 'file-loader'
+                }
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
+}
